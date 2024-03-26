70c0d5555f61dc78d0e8d0b714ae55e5ae9da50d
HOJUN LEE <hppmm@naver.com>
2024-03-26 21:22:48 +0900

    관련 비지니스 로직 작업

--- src/main/java/com/example/wandukong/service/MiniHomeServiceImpl.java
+++ src/main/java/com/example/wandukong/service/MiniHomeServiceImpl.java
@@ -1,19 +1,26 @@
 package com.example.wandukong.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.wandukong.domain.MiniHome.MiniHome;
+import com.example.wandukong.domain.MiniHome.MiniHomeBoard;
 import com.example.wandukong.dto.MiniHomeDto;
+import com.example.wandukong.dto.MiniHome.MiniHomeBoardDto;
+import com.example.wandukong.repository.MiniHomeBoardRepository;
 import com.example.wandukong.repository.MiniHomeRepository;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 public class MiniHomeServiceImpl implements MiniHomeService {
 
     @Autowired
     MiniHomeRepository miniHomeRepository;
+
+    @Autowired
+    MiniHomeBoardRepository miniHomeBoardRepository;
 
     @Override
     public MiniHomeDto getMiniHome(Long userID) {
@@ -24,6 +31,22 @@ public class MiniHomeServiceImpl implements MiniHomeService {
 
         miniHomeDto.setUserID(miniHome.getUserDo().getUserID());
         return miniHomeDto;
+    }
+
+    @Override
+    public List<MiniHomeBoardDto> getBoardList() {
+
+        List<MiniHomeBoard> miniHomeBoards = miniHomeBoardRepository.findAll();
+
+        List<MiniHomeBoardDto> boardList = new ArrayList<>();
+
+        for (MiniHomeBoard miniHomeBoard : miniHomeBoards) {
+            MiniHomeBoardDto miniHomeBoardDto = new MiniHomeBoardDto();
+            miniHomeBoardDto.setBoardID(miniHomeBoard.getBoardID());
+            miniHomeBoardDto.setBoardName(miniHomeBoard.getBoardName());
+        }
+
+        return boardList;
     }
 
 }
