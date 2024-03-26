546952d147871ac0ff48ad7131757fee55821b17
hojun lee <hppmm@naver.com>
2024-03-26 16:46:55 +0900

    내용 추가

--- src/main/java/com/example/wandukong/domain/MiniHome/MiniHomeBoardCategory.java
+++ src/main/java/com/example/wandukong/domain/MiniHome/MiniHomeBoardCategory.java
@@ -1,5 +1,30 @@
 package com.example.wandukong.domain.MiniHome;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "MiniBoardCategory")
 public class MiniHomeBoardCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoryID", unique = true)
+    private Long categoryID;
+
+    @Column(name = "categoryName")
+    private String categoryName;
 
 }
