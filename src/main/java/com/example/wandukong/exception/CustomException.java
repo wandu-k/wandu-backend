6178b58cfcabf6a0f0ab2653c0d0e990e5234173
HOJUN LEE <hppmm@naver.com>
2024-04-01 21:31:46 +0900

    클래스 내부에서 여러 예외를 관리할 수 있도록

--- src/main/java/com/example/wandukong/exception/CustomException.java
+++ src/main/java/com/example/wandukong/exception/CustomException.java
@@ -1,9 +1,10 @@
 package com.example.wandukong.exception;
 
 public class CustomException extends Exception {
-    public class UserNotFoundException extends Exception {
+    public static class UserNotFoundException extends Exception {
         public UserNotFoundException(String message) {
             super(message);
         }
     }
+
 }
