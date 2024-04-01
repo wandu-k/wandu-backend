5784ce1c92720b50fac0e732e9deb0053f8ea5d5
HOJUN LEE <hppmm@naver.com>
2024-04-01 22:31:10 +0900

    시스템 로그를 사용하여 서버에서도 로그를 확인 할 수 있게

--- src/main/java/com/example/wandukong/exception/CustomException.java
+++ src/main/java/com/example/wandukong/exception/CustomException.java
@@ -4,6 +4,7 @@ public class CustomException extends Exception {
     public static class UserNotFoundException extends Exception {
         public UserNotFoundException() {
             // super(message);
+            System.out.println("해당하는 유저가 없습니다.");
         }
     }
 
