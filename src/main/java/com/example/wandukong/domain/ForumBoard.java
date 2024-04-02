3285040f547f1cb7ad86b3ef2abe360c76066fa7
Hyeonwoo-Park <wkdrns5491@gmail.com>
2024-04-02 20:41:34 +0900

    🍱 Add ForumBoard.java
    add board

---
+++ src/main/java/com/example/wandukong/domain/ForumBoard.java
@@ -0,0 +1,34 @@
+package com.example.wandukong.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "ForumBoard")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ForumBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardID;
+
+    private String boardName;
+
+    @OneToMany(mappedBy = "forumBoard")
+    private List<ForumBoard> boards;
+}
