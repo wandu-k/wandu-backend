6e54ce0d63b93fdda7884386b767030a32dce774
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-04-05 23:19:56 +0900

    :white_check_mark: Add CRUD tests to ForumBoardRepositoryTests
    Write unit tests to ensure CRUD operations in ForumBoardRepository.

--- src/test/java/com/example/wandukong/repository/ForumBoardRepositoryTests.java
+++ src/test/java/com/example/wandukong/repository/ForumBoardRepositoryTests.java
@@ -1,9 +1,13 @@
 package com.example.wandukong.repository;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.wandukong.domain.ForumBoard;
 
 @SpringBootTest
 public class ForumBoardRepositoryTests {
@@ -15,7 +19,50 @@ public class ForumBoardRepositoryTests {
   public void nullTest() {
 
     Assertions.assertNotNull(forumBoardRepository);
+
+  }
+
+  @Test
+  public void testInsert() {
     
+    ForumBoard forumBoard = ForumBoard.builder()
+      .boardName("Board")
+      .build();
+
+    forumBoardRepository.save(forumBoard);
+  }
+
+  @Test
+  public void testRead() {
+
+    Long boardID = 1L;
+
+    Optional<ForumBoard> result = forumBoardRepository.findById(boardID);
+
+    result.orElseThrow();
+  }
+
+
+  @Test
+  public void testUpdate() {
+
+    Long boardID = 1L;
+
+    Optional<ForumBoard> result = forumBoardRepository.findById(boardID);
+
+    ForumBoard forumBoard = result.orElseThrow();
+
+    forumBoard.changeBoardName("Update Board");
+
+    forumBoardRepository.save(forumBoard);
+  }
+
+  @Test
+  public void testDelete() {
+    
+    Long boardID = 1L;
+
+    forumBoardRepository.deleteById(boardID);
   }
 
 }
