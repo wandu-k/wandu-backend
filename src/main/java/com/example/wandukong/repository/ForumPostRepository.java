84ebc9335320bedea399c4ff65524619ae1a949b
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-04-07 21:54:31 +0900

    :seedling: Extend ForumPostRepository with ForumPostSearch
    Extend ForumPostRepository interface to inherit additional functionality from ForumPostSearch.

--- src/main/java/com/example/wandukong/repository/ForumPostRepository.java
+++ src/main/java/com/example/wandukong/repository/ForumPostRepository.java
@@ -3,7 +3,8 @@ package com.example.wandukong.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.wandukong.domain.ForumPost;
+import com.example.wandukong.repository.search.ForumPostSearch;
 
-public interface ForumPostRepository extends JpaRepository<ForumPost, Long>{
+public interface ForumPostRepository extends JpaRepository<ForumPost, Long>, ForumPostSearch{
 
 }
