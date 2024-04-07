1c033ef1cdd7a7e4d9d6f5d1190b965428195843
HOJUN LEE <hppmm@naver.com>
2024-04-07 20:06:21 +0900

    🚑 Fixed ApiResponse constructor, object type, and dependency injection error

--- src/main/java/com/example/wandukong/model/ApiResponse.java
+++ src/main/java/com/example/wandukong/model/ApiResponse.java
@@ -1,17 +1,31 @@
 package com.example.wandukong.model;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import lombok.Builder;
 import lombok.Getter;
 
+@Component
 @Getter
 public class ApiResponse {
 
-    private Object object;
-    private int code;
+    private HttpStatus status;
+
+    public ApiResponse() {
+        // 기본 생성자는 일반적으로 비워둡니다.
+    }
+
+    // 생성자
+    public ApiResponse(HttpStatus status) {
+        this.status = status;
+    }
+
     private String message;
 
-    public ApiResponse(Object object, int code, String message) {
-        this.object = object;
-        this.code = code;
+    @Builder
+    public ApiResponse(HttpStatus status, String message) {
+        this.status = status;
         this.message = message;
     }
 }
