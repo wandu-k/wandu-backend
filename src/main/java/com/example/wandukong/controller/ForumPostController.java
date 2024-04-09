7dc58d274e0912c18636c5d798493731a78f234d
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-04-10 06:16:53 +0900

    :sparkles: Add POST, PUT, DELETE mappings to ForumPostController
    Implement POST, PUT, DELETE mappings in ForumPostController to handle creation, updating, and deletion of ForumPost entities.

--- src/main/java/com/example/wandukong/controller/ForumPostController.java
+++ src/main/java/com/example/wandukong/controller/ForumPostController.java
@@ -1,7 +1,11 @@
 package com.example.wandukong.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +13,7 @@ import com.example.wandukong.dto.ForumPostDto;
 import com.example.wandukong.dto.PageRequestDto;
 import com.example.wandukong.dto.PageResponseDto;
 import com.example.wandukong.service.ForumPostService;
-
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -31,5 +35,28 @@ public class ForumPostController {
     return forumPostService.getList(pageRequestDto);
   }
 
-  
+  @PostMapping("/")
+  public Map<String, Long> register(@RequestBody ForumPostDto forumPostDto) {
+
+    Long postID = forumPostService.register(forumPostDto);
+
+    return Map.of("postID", postID);
+  }
+
+  @PutMapping("/{postID}")
+  public Map<String, String> modify(@PathVariable Long postID, @RequestBody ForumPostDto forumPostDto) {
+
+    forumPostDto.setPostID(postID);
+
+    forumPostService.modify(forumPostDto);
+
+    return Map.of("RESULT", "SUCCESS");
+  }
+
+  @DeleteMapping("/{postID}")
+  public Map<String, String> remove(@PathVariable Long postID) {
+    forumPostService.remove(postID);
+
+    return Map.of("RESULT", "SUCCESS");
+  }
 }
