9edcd030be4e051ec316a29cbca1eb9cae7e2cda
jiwook <wlwl45wl@gmail.com>
2024-04-30 16:19:57 +0900

    ✨shop

--- src/main/java/com/example/wandukong/repository/ShopInfo/ItemFileRepository.java
+++ src/main/java/com/example/wandukong/repository/ShopInfo/ItemFileRepository.java
@@ -1,11 +1,10 @@
 package com.example.wandukong.repository.ShopInfo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.wandukong.domain.ShopInfo.ItemFile;
+import com.example.wandukong.domain.ShopInfo.Shop;
 
-@Repository
-public interface ItemFileRepository extends JpaRepository<ItemFile, Long> {
+public interface ItemFileRepository extends JpaRepository<ItemFile, Shop> {
 
 }
