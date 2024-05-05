9f001740e4cb891a66eca4feec3836aac08ef46b
HOJUN LEE <hppmm@naver.com>
2024-05-06 01:03:06 +0900

    일일 방문자 자정 리셋 기능

--- src/main/java/com/example/wandukong/WandukongApplication.java
+++ src/main/java/com/example/wandukong/WandukongApplication.java
@@ -1,10 +1,19 @@
 package com.example.wandukong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class WandukongApplication {
+
+	@Autowired
+	RedisTemplate<String, String> redisTemplate;
+
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
@@ -12,6 +21,11 @@ public class WandukongApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(WandukongApplication.class, args);
+	}
+
+	@Scheduled(cron = "0 0 0 * * *")
+	public void resetTodayVisit() {
+		redisTemplate.delete("viewKey");
 	}
 
 }
