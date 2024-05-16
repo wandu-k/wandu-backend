30dcea7e630601e05c6392577e3e3c07a2d14a0b
LEE HOJUN <hppmm@naver.com>
2024-05-16 10:41:44 +0900

    missing file upload

--- src/main/java/com/example/wandukong/repository/FriendRepository.java
+++ src/main/java/com/example/wandukong/repository/FriendRepository.java
@@ -1,9 +1,15 @@
 package com.example.wandukong.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.wandukong.domain.Friend;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    List<Friend> findAllByUserId(Long userId);
+
+    List<Friend> findAllByFriendId(Long userId);
 
 }
