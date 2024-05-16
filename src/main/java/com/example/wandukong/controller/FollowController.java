1f71ef864d578e11e50da97343f8b69d67586e5c
LEE HOJUN <hppmm@naver.com>
2024-05-16 10:56:50 +0900

    🎨 swagger doc work for follow controller

--- src/main/java/com/example/wandukong/controller/FollowController.java
+++ src/main/java/com/example/wandukong/controller/FollowController.java
@@ -7,6 +7,10 @@ import com.example.wandukong.dto.FriendDto;
 import com.example.wandukong.exception.CustomException.BadRequestException;
 import com.example.wandukong.service.FollowService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "팔로우", description = "팔로우 API")
 @RestController
 @RequestMapping("/api/user/follow")
 public class FollowController {
@@ -27,6 +32,8 @@ public class FollowController {
     @Autowired
     FollowService followService;
 
+    @Operation(summary = "팔로잉 리스트", description = "유저 번호로 팔로잉 리스트를 가져옵니다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/following")
     public ResponseEntity<?> getFollowingList(@RequestParam Long userId) {
 
@@ -35,6 +42,8 @@ public class FollowController {
         return new ResponseEntity<>(friends, HttpStatus.OK);
     }
 
+    @Operation(summary = "팔로워 리스트", description = "유저 번호로 팔로워 리스트를 가져옵니다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/follower")
     public ResponseEntity<?> getFollowerList(@RequestParam Long userId) {
         List<FriendDto> friends = followService.getFollowerList(userId);
@@ -42,6 +51,8 @@ public class FollowController {
         return new ResponseEntity<>(friends, HttpStatus.OK);
     }
 
+    @Operation(summary = "팔로잉", description = "팔로잉")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping
     public ResponseEntity<?> following(@AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody FriendDto friendDto) throws BadRequestException {
@@ -55,6 +66,8 @@ public class FollowController {
         return new ResponseEntity<>("팔로우 성공", HttpStatus.OK);
     }
 
+    @Operation(summary = "팔로잉 취소", description = "팔로잉 취소")
+    @SecurityRequirement(name = "Bearer Authentication")
     @DeleteMapping
     public ResponseEntity<?> unFollowing(@AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody FriendDto friendDto) throws BadRequestException {
