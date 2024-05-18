1327b61347b859fabe27533e4ad188bdb9e01112
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-19 06:45:29 +0900

    :card_file_box: Update entity structure
    Refactored entity-related code for better performance

--- src/main/java/com/example/wandukong/domain/ForumPost.java
+++ src/main/java/com/example/wandukong/domain/ForumPost.java
@@ -2,58 +2,56 @@ package com.example.wandukong.domain;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "ForumPost")
-@ToString
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ForumPost {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boardId")
     private ForumBoard forumBoard;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
     private UserDo userDo;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "content")
     private String content;
 
+    @Column(name = "writeDate")
+    @CreationTimestamp
     private LocalDate writeDate;
 
+    @Column(name = "state")
     private int state;
 
-    public void changeTitle(String title) {
+    @Builder
+    public ForumPost(Long postId, ForumBoard forumBoard, UserDo userDo, String title, String content, LocalDate writeDate, int state) {
+        this.postId = postId;
+        this.forumBoard = forumBoard;
+        this.userDo = userDo;
         this.title = title;
-    }
-
-    public void changeContent(String content) {
         this.content = content;
-    }
-
-    public void changeWriteDate(LocalDate writeDate) {
         this.writeDate = writeDate;
+        this.state = state;
     }
 
-    public void changeState(int state) {
+    public void changePost(ForumBoard forumBoard, String title, String content, int state) {
+        this.forumBoard = forumBoard;
+        this.title = title;
+        this.content = content;
         this.state = state;
     }
 
