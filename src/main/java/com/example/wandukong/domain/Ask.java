40e03ee054c011e24652ee3f1353e23ede83d5ff
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-20 16:52:52 +0900

    :hammer: Update Ask entity
    Updated annotations for improved ORM mapping, Added new methods for handling updated fields

--- src/main/java/com/example/wandukong/domain/Ask.java
+++ src/main/java/com/example/wandukong/domain/Ask.java
@@ -1,21 +1,17 @@
 package com.example.wandukong.domain;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "Ask")
@@ -26,8 +22,9 @@ public class Ask {
     @Column(name = "askId", unique = true)
     private Long askId;
 
-    @Column(name = "userId")
-    private String userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private UserDo userDo;
 
     @Column(name = "title")
     private String title;
@@ -36,11 +33,46 @@ public class Ask {
     private String content;
 
     @Column(name = "writeDate")
-    private Date writeDate;
+    @CreationTimestamp
+    private LocalDate writeDate;
 
     @Column(name = "solveState")
     private int solveState;
 
     @Column(name = "hideState")
     private int hideState;
+
+    @Column(name = "count", columnDefinition = "int default 0")
+    private int count;
+
+    @OneToMany(mappedBy = "Ask", cascade = CascadeType.ALL)
+    private List<AdminComment> adminComments = new ArrayList<>();
+
+    @Builder
+    public Ask(Long askId, UserDo userDo, String title, String content, LocalDate writeDate, int solveState, int hideState, int count, List<AdminComment> adminComments) {
+        this.askId = askId;
+        this.userDo = userDo;
+        this.title = title;
+        this.content = content;
+        this.writeDate = writeDate;
+        this.solveState = solveState;
+        this.hideState = hideState;
+        this.count = count;
+        this.adminComments = adminComments;
+    }
+
+    public void changeAsk(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void changeState(int solveState, int hideState) {
+        this.solveState = solveState;
+        this.hideState = hideState;
+    }
+
+    public void changeCount(int count) {
+        this.count = count++;
+    }
+
 }
