62e0eff77ad024998b17483460223cde568465b4
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-20 16:51:11 +0900

    :sparkles: Create AdminComment entity
    Added AdminComment entity with necessary fields

---
+++ src/main/java/com/example/wandukong/domain/AdminComment.java
@@ -0,0 +1,50 @@
+package com.example.wandukong.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "AdminComment")
+public class AdminComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "commentId", unique = true)
+    private int commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private UserDo userDo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "askId")
+    private Ask ask;
+
+    @Column(name = "commentContent")
+    private String commentContent;
+
+    @CreationTimestamp
+    @Column(name = "writeDate")
+    private LocalDate writeDate;
+
+    @Builder
+    public AdminComment(int commentId, UserDo userDo, Ask ask, String commentContent, LocalDate writeDate) {
+        this.commentId = commentId;
+        this.userDo = userDo;
+        this.ask = ask;
+        this.commentContent = commentContent;
+        this.writeDate = writeDate;
+    }
+
+    public void changeContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+}
