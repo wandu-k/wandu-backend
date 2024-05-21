cb6f0d2089d934938a070cf2dc0af2948373d2a4
LEE HOJUN <hppmm@naver.com>
2024-05-21 11:34:08 +0900

    Configuration QueryDslConfig

--- src/main/java/com/example/wandukong/repository/user/UserRepository.java
+++ src/main/java/com/example/wandukong/repository/user/UserRepository.java
@@ -1,10 +1,8 @@
 package com.example.wandukong.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.example.wandukong.domain.UserDo;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserDo, Long>, UserRepositoryCustom {
 
     UserDo findByEmail(String username);
