7afac4e0511f7197d2e0bea88463be1af21514b5
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-26 21:09:09 +0900

    :truck: Refactor AdminCommentRepository package

--- src/main/java/com/example/wandukong/repository/ask/AdminCommentRepository.java
+++ src/main/java/com/example/wandukong/repository/ask/AdminCommentRepository.java
@@ -1,6 +1,6 @@
-package com.example.wandukong.repository;
+package com.example.wandukong.repository.ask;
 
-import com.example.wandukong.domain.AdminComment;
+import com.example.wandukong.domain.ask.AdminComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminCommentRepository extends JpaRepository<AdminComment, Long> {
