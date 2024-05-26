6de9bd3dbd940c66947433a2960bf1a52af6a438
LEE HOJUN <hppmm@naver.com>
2024-05-26 17:08:52 +0900

    ♻️ Login Fix

---
+++ src/main/java/com/example/wandukong/domain/MiniHome/MiniHomeDiary.java
@@ -0,0 +1,69 @@
+package com.example.wandukong.domain.MiniHome;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.example.wandukong.domain.UserDo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "MiniHomeDiary")
+public class MiniHomeDiary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "postId", unique = true)
+    private Long postId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private UserDo userDo;
+
+    @ManyToOne
+    @JoinColumn(name = "hpId")
+    private MiniHome miniHome;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "writeDay")
+    private LocalDate writeDay;
+
+    @Builder
+    public MiniHomeDiary(Long postId, UserDo userDo, MiniHome miniHome,
+            String title,
+            String content,
+            LocalDate writeDay) {
+        this.postId = postId;
+        this.userDo = userDo;
+        this.miniHome = miniHome;
+        this.title = title;
+        this.content = content;
+        this.writeDay = writeDay;
+    }
+
+    // 필드 업데이트 메서드
+    public void updatePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
--- src/main/java/com/example/wandukong/dto/MiniHome/MiniHomeDiaryDto.java
+++ src/main/java/com/example/wandukong/dto/MiniHome/MiniHomeDiaryDto.java
@@ -1,5 +1,28 @@
 package com.example.wandukong.dto.MiniHome;
 
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class MiniHomeDiaryDto {
 
+    private Long postId;
+    private Long userId;
+    private Long hpId;
+    private String title;
+    private String content;
+    private LocalDate writeDay;
+
+    @Builder
+    public MiniHomeDiaryDto(Long postId, Long userId, Long hpId, String title, String content,
+            LocalDate writeDay) {
+        this.postId = postId;
+        this.userId = userId;
+        this.hpId = hpId;
+        this.title = title;
+        this.content = content;
+        this.writeDay = writeDay;
+    }
 }
