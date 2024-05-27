361eb18b4aa2ee937b1ad58f78eee2ef3af50696
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-27 09:48:15 +0900

    :sparkles: Create repository for forumPostReview

---
+++ src/main/java/com/example/wandukong/repository/forum/ForumPostReviewRepository.java
@@ -0,0 +1,7 @@
+package com.example.wandukong.repository.forum;
+
+import com.example.wandukong.domain.forum.ForumPostReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForumPostReviewRepository extends JpaRepository<ForumPostReview, Long> {
+}
