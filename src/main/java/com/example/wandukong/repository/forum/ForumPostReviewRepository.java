de5d99748844553dd66d8d50d1a76098783fe10e
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-29 11:40:15 +0900

    :hammer: Update ForumPostReviewRepository

--- src/main/java/com/example/wandukong/repository/forum/ForumPostReviewRepository.java
+++ src/main/java/com/example/wandukong/repository/forum/ForumPostReviewRepository.java
@@ -1,7 +1,8 @@
 package com.example.wandukong.repository.forum;
 
 import com.example.wandukong.domain.forum.ForumPostReview;
+ import com.example.wandukong.repository.forum.search.ForumPostReviewSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ForumPostReviewRepository extends JpaRepository<ForumPostReview, Long> {
+public interface ForumPostReviewRepository extends JpaRepository<ForumPostReview, Long>, ForumPostReviewSearch {
 }
