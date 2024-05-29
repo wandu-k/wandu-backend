0468d7ede58f9bca49244813a0bf1a5f9dbe96df
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-05-29 20:34:02 +0900

    :hammer: Update AdminCommentRepository
    extends AdminCommentSearch

--- src/main/java/com/example/wandukong/repository/ask/AdminCommentRepository.java
+++ src/main/java/com/example/wandukong/repository/ask/AdminCommentRepository.java
@@ -1,7 +1,8 @@
 package com.example.wandukong.repository.ask;
 
 import com.example.wandukong.domain.ask.AdminComment;
+import com.example.wandukong.repository.ask.search.AdminCommentSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminCommentRepository extends JpaRepository<AdminComment, Long> {
+public interface AdminCommentRepository extends JpaRepository<AdminComment, Long>, AdminCommentSearch {
 }
