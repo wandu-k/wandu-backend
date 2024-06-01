ecf4022bdfc621ec930218e3fd60dd671fc0c8da
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-06-01 17:52:36 +0900

    :sparkles:  Add file upload functionality to ForumPostController

--- src/main/java/com/example/wandukong/controller/forum/ForumPostController.java
+++ src/main/java/com/example/wandukong/controller/forum/ForumPostController.java
@@ -1,5 +1,6 @@
 package com.example.wandukong.controller.forum;
 
+import java.io.IOException;
 import java.util.Objects;
 
 import com.example.wandukong.dto.CustomUserDetails;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,7 @@ import com.example.wandukong.exception.CustomException.PostNotFoundException;
 import com.example.wandukong.service.forum.ForumPostService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "자유 게시글", description = "자유 게시글 API")
 @RestController
@@ -32,6 +35,7 @@ public class ForumPostController {
   private final ForumPostService forumPostService;
 
   @Operation(summary = "자유게시판 번호로 조회")
+  @SecurityRequirement(name = "Baerer Authentication")
   @GetMapping
   public ResponseEntity<?> get(@RequestParam Long postId) throws PostNotFoundException {
 
@@ -41,6 +45,7 @@ public class ForumPostController {
   }
 
   @Operation(summary = "자유게시판 리스트 조회")
+  @SecurityRequirement(name = "Baerer Authentication")
   @PostMapping
   public ResponseEntity<?> list(@RequestBody PageRequestDto pageRequestDto) {
 
@@ -51,16 +56,17 @@ public class ForumPostController {
 
   @Operation(summary = "자유게시판 등록 및 수정")
   @SecurityRequirement(name = "Bearer Authentication")
-  @PutMapping
+  @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<?> modify(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-      @RequestBody ForumPostDto forumPostDto)
-      throws BoardNotFoundException, BadRequestException {
+      @RequestPart(required = false, value = "postFile") MultipartFile postFile,
+      @RequestPart ForumPostDto forumPostDto)
+      throws BoardNotFoundException, BadRequestException, IOException {
 
     if (!Objects.equals(customUserDetails.getAccountDto().getUserId(), forumPostDto.getUserId())) {
       throw new BadRequestException();
     }
 
-    ApiResponseDto apiResponse = forumPostService.modify(forumPostDto);
+    ApiResponseDto apiResponse = forumPostService.modify(postFile, forumPostDto);
 
     return new ResponseEntity<>(apiResponse.getMessage(), apiResponse.getStatus());
   }
