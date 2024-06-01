0eaa5957acc844ebc4fb5e28ea8d097237ba05d6
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-06-01 17:07:01 +0900

    :sparkles: Create ForumPostFileRepository interface

---
+++ src/main/java/com/example/wandukong/repository/forum/ForumPostFileRepository.java
@@ -0,0 +1,7 @@
+package com.example.wandukong.repository.forum;
+
+import com.example.wandukong.domain.forum.ForumPostFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForumPostFileRepository extends JpaRepository<ForumPostFile, Long> {
+}
