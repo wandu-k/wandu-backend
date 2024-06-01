737535d7aba564258b7020e0d29a37b3fdf132d0
Hyeon-wooPark <wkdrns5491@gmail.com>
2024-06-01 17:06:17 +0900

    :sparkles: Create AskFileRepository interface

---
+++ src/main/java/com/example/wandukong/repository/ask/AskFileRepository.java
@@ -0,0 +1,7 @@
+package com.example.wandukong.repository.ask;
+
+import com.example.wandukong.domain.ask.AskFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AskFileRepository extends JpaRepository<AskFile, Long> {
+}
