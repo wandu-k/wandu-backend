03491eec4e45ad1ce42f625b0a1ac520017901a4
jwba55 <118268966+jwba55@users.noreply.github.com>
2024-09-26 13:16:52 +0900

    Update WebConfig.java add description

--- src/main/java/com/example/wandukong/config/WebConfig.java
+++ src/main/java/com/example/wandukong/config/WebConfig.java
@@ -16,3 +16,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With");
     }
 }
+
+//같은 서버에 프론트, 백엔드가 같이 있으면 상관없는데 주소가 다를 경우에 한 곳에서 작동되도록 허용하겠다고 선언하는것
