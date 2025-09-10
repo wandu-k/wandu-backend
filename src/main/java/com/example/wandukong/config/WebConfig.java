92e968524137940245a2500c1108c0f505a114ab
jwba55 <wlwl45wl@gmail.com>
2025-09-10 15:04:06 +0900

    test

--- src/main/java/com/example/wandukong/config/WebConfig.java
+++ src/main/java/com/example/wandukong/config/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://mgsip.xyz:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("Authorization", "Content-Type", "X-Requested-With");
+                .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
+                .exposedHeaders("Authorization"); // ★ 프론트에서 읽을 수 있도록 노출
     }
 }
 
-//같은 서버에 프론트, 백엔드가 같이 있으면 상관없는데 주소가 다를 경우에 한 곳에서 작동되도록 허용하겠다고 선언하는것
+// 같은 서버에 프론트, 백엔드가 같이 있으면 상관없는데 주소가 다를 경우에 한 곳에서 작동되도록 허용하겠다고 선언하는것
